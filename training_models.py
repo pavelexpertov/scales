de53b681775531d5619bf674e3a88cf7373ea6bb
pavelexpertov <pavelm01@hotmail.co.uk>
2020-01-03 14:12:53 +0000

    Finish up the training_models.py

--- training_models.py
+++ training_models.py
@@ -18,17 +18,17 @@ y = df.loc[:, ['C']]
 clf = DecisionTreeClassifier()
 scores = cross_val_score(clf, X, y, cv=3)
 print('With 3 cross_validation', 'Mean:', scores.mean(), scores.std())
-recording_list.append(['With 3 cross_validation', 'Mean:', scores.mean(), scores.std()])
+recording_list.append(['With 3 cross_validation', scores.mean(), scores.std()])
 
 clf = DecisionTreeClassifier()
 scores = cross_val_score(clf, X, y, cv=5)
 print('With 5 cross_validation', 'Mean:', scores.mean(), scores.std())
-recording_list.append(['With 5 cross_validation', 'Mean:', scores.mean(), scores.std()])
+recording_list.append(['With 5 cross_validation', scores.mean(), scores.std()])
 
 clf = DecisionTreeClassifier()
 scores = cross_val_score(clf, X, y, cv=10)
 print('With 10 cross_validation', 'Mean:', scores.mean(), scores.std())
-recording_list.append(['With 10 cross_validation', 'Mean:', scores.mean(), scores.std()])
+recording_list.append(['With 10 cross_validation', scores.mean(), scores.std()])
 
 
 # <markdown>
@@ -58,67 +58,79 @@ encoded_y = enc_y.transform(np.ravel(y.to_numpy()))
 clf = LogisticRegression(random_state=0)
 scores = cross_val_score(clf, X, encoded_y, cv=10)
 print('With LogisticRegression', 'Mean:', scores.mean(), scores.std())
-recording_list.append(['With LogisticRegression', 'Mean:', scores.mean(), scores.std()])
+recording_list.append(['With LogisticRegression', scores.mean(), scores.std()])
 
 # In [ ]
 from sklearn.linear_model import SGDClassifier
 clf = SGDClassifier(max_iter=1000, tol=1e-3)
 scores = cross_val_score(clf, X, encoded_y, cv=10)
 print('With SGDClassifier', 'Mean:', scores.mean(), scores.std())
-recording_list.append(['With SGDClassifier', 'Mean:', scores.mean(), scores.std()])
+recording_list.append(['With SGDClassifier', scores.mean(), scores.std()])
 
 # In [ ]
 from sklearn.svm import SVC
 clf = SVC(gamma='auto')
 scores = cross_val_score(clf, X, encoded_y, cv=10)
 print('With SVC', 'Mean:', scores.mean(), scores.std())
-recording_list.append(['With SVC', 'Mean:', scores.mean(), scores.std()])
+recording_list.append(['With SVC', scores.mean(), scores.std()])
 
 # In [ ]
 from sklearn.svm import LinearSVC
 clf = LinearSVC(max_iter=5000)
 scores = cross_val_score(clf, X, encoded_y, cv=10)
 print('With LinearSVC', 'Mean:', scores.mean(), scores.std())
-recording_list.append(['With LinearSVC', 'Mean:', scores.mean(), scores.std()])
+recording_list.append(['With LinearSVC', scores.mean(), scores.std()])
 
 # In [ ]
 from sklearn.ensemble import RandomForestClassifier
 clf = RandomForestClassifier(n_estimators=200)
 scores = cross_val_score(clf, X, encoded_y, cv=10)
 print('With RandomForestClassifier', 'Mean:', scores.mean(), scores.std())
-recording_list.append(['With RandomForestClassifier', 'Mean:', scores.mean(), scores.std()])
+recording_list.append(['With RandomForestClassifier', scores.mean(), scores.std()])
 
 # In [ ]
 from sklearn.naive_bayes import CategoricalNB
 clf = CategoricalNB()
 scores = cross_val_score(clf, X, encoded_y, cv=10)
 print('With CategoricalNB', 'Mean:', scores.mean(), scores.std())
-recording_list.append(['With CategoricalNB', 'Mean:', scores.mean(), scores.std()])
+recording_list.append(['With CategoricalNB', scores.mean(), scores.std()])
 
 # In [ ]
 from sklearn.neighbors import NearestCentroid
 clf = NearestCentroid()
 scores = cross_val_score(clf, X, encoded_y, cv=10)
 print('With NearestCentroid', 'Mean:', scores.mean(), scores.std())
-recording_list.append(['With NearestCentroid', 'Mean:', scores.mean(), scores.std()])
+recording_list.append(['With NearestCentroid', scores.mean(), scores.std()])
 
 # In [ ]
 from sklearn.neighbors import KNeighborsClassifier
 clf = KNeighborsClassifier()
 scores = cross_val_score(clf, X, encoded_y, cv=10)
 print('With KNeighborsClassifier', 'Mean:', scores.mean(), scores.std())
-recording_list.append(['With KNeighborsClassifier', 'Mean:', scores.mean(), scores.std()])
+recording_list.append(['With KNeighborsClassifier', scores.mean(), scores.std()])
 
 # In [ ]
 from sklearn.neighbors import RadiusNeighborsClassifier
 clf = RadiusNeighborsClassifier(radius=2.0)
 scores = cross_val_score(clf, X, encoded_y, cv=10)
 print('With RadiusNeighborsClassifier', 'Mean:', scores.mean(), scores.std())
-recording_list.append(['With RadiusNeighborsClassifier', 'Mean:', scores.mean(), scores.std()])
+recording_list.append(['With RadiusNeighborsClassifier', scores.mean(), scores.std()])
 
 # In [ ]
 from sklearn.neural_network import MLPClassifier
-clf = MLPClassifier(max_iter=1100)
+clf = MLPClassifier(max_iter=1200)
 scores = cross_val_score(clf, X, encoded_y, cv=10)
 print('With MLPClassifier', 'Mean:', scores.mean(), scores.std())
-recording_list.append(['With MLPClassifier', 'Mean:', scores.mean(), scores.std()])
+recording_list.append(['With MLPClassifier', scores.mean(), scores.std()])
+
+# In [ ]
+sorted_list = sorted(recording_list, key=lambda item: item[1], reverse=True)
+for classifier, mean, std in sorted_list:
+    print(classifier, 'Mean:', mean, 'std:', std)
+
+# <markdown>
+After experimenting with 11 algorithms, it looks like a neural network type classifier wins the testing round.
+
+Now I need to perform two things:
+    - I will need to delve further into algorithms to understand how they work as to have an idea why they perform either poorly or brilliantly.
+    - During testing, I have been wondering about the current dataset and I think I have managed to notice few descrepencies I didn't see before. I will elaborate on this later.
